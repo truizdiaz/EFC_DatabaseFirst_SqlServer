8c431cee3e7a93015d4a0177f3a8d085209807c1
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-25 18:34:38 -0300

    add code

---
+++ EFCDatabaseFirst/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using EFCDatabaseFirst.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCDatabaseFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listCustomers = await _context.Customers.ToListAsync();
+                return Ok(listCustomers);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
